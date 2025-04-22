cbbe7e76254b9b92f8c59ff06edc9f0cf0e65fb4
heinekmadsen <48906528+heinekmadsen@users.noreply.github.com>
2025-04-22 13:13:38 +0200

    Trying to update the mode of the climate entity based on the heater in the spa

--- components/balboa_spa/climate/balboa_climate.cpp
+++ components/balboa_spa/climate/balboa_climate.cpp
@@ -22,11 +22,16 @@ namespace esphome
     void BalboaClimate::setup()
     {
       this->parent_->register_sensor_callback(40, [this](const float temp)
-                                              { this->target_temperature = temp; this->publish_state();});
+                                              { this->target_temperature = temp; this->publish_state(); });
       this->parent_->register_sensor_callback(41, [this](const float temp)
-                                              { 
-                                                this->current_temperature = temp; 
-                                              this->publish_state(); });
+                                              { this->current_temperature = temp; this->publish_state(); });
+
+      // Register callback for heater state
+      this->parent_->register_binary_sensor_callback(21, [this](const bool state)
+                                                     {
+                                                       this->action = state ? climate::CLIMATE_ACTION_HEATING : climate::CLIMATE_ACTION_IDLE;
+                                                       this->publish_state();
+                                                     });
     }
     void BalboaClimate::control(const climate::ClimateCall &call)
     {
